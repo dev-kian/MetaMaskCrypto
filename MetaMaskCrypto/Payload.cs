0c815b76bbf7320667649f9027785c4add88be49
dev-kian <kianshabanpourr@gmail.com>
2023-06-01 11:21:01 +0330

    Add project files.

---
+++ MetaMaskCrypto/Payload.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MetaMaskCrypto;
+
+internal class Payload
+{
+    [JsonPropertyName("data")]
+    [JsonInclude]
+    public string Data { get; private set; }
+    [JsonPropertyName("iv")]
+    [JsonInclude]
+    public string IV { get; private set; }
+    [JsonPropertyName("salt")]
+    [JsonInclude]
+    public string Salt { get; private set; }
+}
